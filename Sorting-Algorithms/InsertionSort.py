52effeb594eda89e04729b8aa1a23104731d7458
Sanjivan <sanjivan.nithiraj@gmail.com>
2025-01-15 00:50:40 +0530

    Insertion sort code

---
+++ Sorting-Algorithms/InsertionSort.py
@@ -0,0 +1,21 @@
+def insertion_sort(inputArray: list[int], Ascending: bool = True) -> list[int]:
+    length = len(inputArray)
+    if(length==1): return inputArray
+    outputArray = list(inputArray)
+    for i in range(1,length):
+        for j in range(i,0,-1):
+            if(Ascending == (outputArray[j] > outputArray[j-1])):
+                break
+            swap(outputArray,j,j-1)
+    return outputArray
+            
+            
+            
+def swap(inputArray: list[int], index1: int, index2: int) ->None:
+    temp = inputArray[index1]
+    inputArray[index1] = inputArray[index2]
+    inputArray[index2] = temp
+
+
+print(insertion_sort([6,5,4,3,2,1]))
+print(insertion_sort([1,2,3,4,5,6],False))
