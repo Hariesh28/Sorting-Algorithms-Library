518d18fb2cfcaac1b4485eb399b72f38c3714761
Hariesh <hariesh23110344@snuchennai.edu.in>
2025-01-16 20:41:03 +0530

    Added doc for swap

--- sorting_techniques/_utils.py
+++ sorting_techniques/_utils.py
@@ -26,7 +26,31 @@ def is_sorted(array: list | tuple, ascending: bool = True) -> bool:
     comparator = (lambda x, y: x <= y) if ascending else (lambda x, y: x >= y)
     return all(comparator(array[i], array[i + 1]) for i in range(len(array) - 1))
 
-def swap(inputArray: list[int], index1: int, index2: int) ->None:
+def swap(inputArray: list[int], index1: int, index2: int) -> None:
+
+    """
+    Swaps the elements at the specified indices in the given list.
+
+    Parameters:
+        inputArray (list[int]): The list of integers in which the elements will be swapped.
+        index1 (int): The index of the first element to swap.
+        index2 (int): The index of the second element to swap.
+
+    Returns:
+        None: This function modifies the input list in place and does not return anything.
+
+    Examples:
+        >>> arr = [1, 2, 3, 4]
+        >>> swap(arr, 0, 3)
+        >>> arr
+        [4, 2, 3, 1]
+
+        >>> arr = [10, 20, 30, 40]
+        >>> swap(arr, 1, 2)
+        >>> arr
+        [10, 30, 20, 40]
+    """
+
     temp = inputArray[index1]
     inputArray[index1] = inputArray[index2]
     inputArray[index2] = temp
