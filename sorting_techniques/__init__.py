b71ba77e93427559a2c5c8a863247cb28edbbd99
Hariesh <hariesh23110344@snuchennai.edu.in>
2025-01-25 20:31:11 +0530

    Add Counting Sort

--- sorting_techniques/__init__.py
+++ sorting_techniques/__init__.py
@@ -1,9 +1,10 @@
 from .bogo_sort import bogo_sort
-from .insertion_sort import insertion_sort
-from .selection_sort import selection_sort
 from .heap_sort import heap_sort
-from .merge_sort import merge_sort
 from .quick_sort import quick_sort
+from .merge_sort import merge_sort
+from .counting_sort import counting_sort
+from .selection_sort import selection_sort
+from .insertion_sort import insertion_sort
 from ._utils import generate_random_numbers
 
 __all__ = [
@@ -13,5 +14,6 @@ __all__ = [
     "heap_sort",
     "heap_sort",
     "merge_sort",
-    "quick_sort"
+    "quick_sort",
+    "counting_sort"
 ]
