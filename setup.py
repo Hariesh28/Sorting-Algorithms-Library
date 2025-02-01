52b38ee7d3815309bf854049f1c8e0b45ae6e581
Hariesh <hariesh23110344@snuchennai.edu.in>
2025-02-01 14:08:57 +0530

    Fix: license classifier

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.9',
